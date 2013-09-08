34e1fb7f6f3a23c1359101d08995965aec894585
Joe Nelson <cred+github@begriffs.com>
2013-09-08 15:11:40 -0700

    WIP: include working insertion

--- postgresql-simple/two-and-two.hs
+++ postgresql-simple/two-and-two.hs
@@ -1,10 +1,24 @@
- module Main where
+module Main where
 
- import Database.PostgreSQL.Simple
- import Control.Monad
+import Database.PostgreSQL.Simple
+import Control.Monad
+import Control.Applicative
 
- main = do
-   conn <- connect defaultConnectInfo
-   xs   <- query_ conn "select 2 + 2"
-   forM_ xs $ \(Only i) ->
-     putStrLn $ show (i :: Int)
+printOnlies :: (Show a) => [Only a] -> IO ()
+printOnlies q =
+  forM_ q $ \(Only i) ->
+    putStrLn $ show i
+
+main = do
+  conn <- connect defaultConnectInfo
+
+  putStrLn "2 + 2"
+  printOnlies =<< (query_ conn "select 2 + 2" :: IO [Only Int])
+
+  putStrLn "3 + 5"
+  printOnlies =<< (query conn "select ? + ?" (3 :: Int, 5 :: Int) :: IO [Only Int])
+
+  putStrLn "Enter a word"
+  word <- getLine :: IO String
+  (query conn "insert into words (word) values (?)" $ Only word) :: IO [Only Int]
+  return ()
