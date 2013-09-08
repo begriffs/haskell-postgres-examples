e45a93c77b9b7587a44039584a32bc649fb10fbb
Joe Nelson <cred+github@begriffs.com>
2013-09-08 16:33:27 -0700

    Overspecify argument type to simplify the rest of the code

--- postgresql-simple/two-and-two.hs
+++ postgresql-simple/two-and-two.hs
@@ -4,7 +4,7 @@ import Database.PostgreSQL.Simple
 import Control.Monad
 import Control.Applicative
 
-printOnlies :: (Show a) => [Only a] -> IO ()
+printOnlies :: [Only Int] -> IO ()
 printOnlies q =
   forM_ q $ \(Only i) ->
     putStrLn $ show i
@@ -13,12 +13,11 @@ main = do
   conn <- connect defaultConnectInfo
 
   putStrLn "2 + 2"
-  printOnlies =<< (query_ conn "select 2 + 2" :: IO [Only Int])
+  printOnlies =<< query_ conn "select 2 + 2"
 
   putStrLn "3 + 5"
-  printOnlies =<< (query conn "select ? + ?" (3 :: Int, 5 :: Int) :: IO [Only Int])
+  printOnlies =<< query conn "select ? + ?" (3 :: Int, 5 :: Int)
 
   putStrLn "Enter a word"
   word <- getLine :: IO String
-  (query conn "insert into words (word) values (?)" $ Only word) :: IO [Only Int]
-  return ()
+  query conn "insert into words (word) values (?)" $ Only word :: IO [Only Int]
