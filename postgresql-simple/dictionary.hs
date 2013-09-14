37a9f0750f0bbace32f6bc714efef465835980a6
Joe Nelson <cred+github@begriffs.com>
2013-09-13 22:27:05 -0700

    Add saving via ToRow

--- postgresql-simple/dictionary.hs
+++ postgresql-simple/dictionary.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.ToRow
+import Database.PostgreSQL.Simple.ToField
 import Data.Maybe
 import Control.Monad
 import Control.Applicative
@@ -11,7 +13,16 @@ data Dictionary = Dictionary { word :: Maybe String, definition :: Maybe String 
 instance FromRow Dictionary where
   fromRow = Dictionary <$> field <*> field
 
+instance ToRow Dictionary where
+  toRow d = [toField (word d), toField (definition d)]
+
 main = do
   conn <- connect defaultConnectInfo { connectDatabase = "haskell" }
 
   mapM_ print =<< ( query_ conn "select word, definition from words" :: IO [Dictionary] )
+
+  putStrLn "Enter a new word"
+  word <- getLine
+  putStrLn "What does it mean?"
+  def <- getLine
+  execute conn "insert into words (word, definition) values (?, ?)" $ Dictionary (Just word) (Just def)
